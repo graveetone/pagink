41692d8ac153e4557f2b708a8c43b8fc286edece
graveetone <zastavskijs@gmail.com>
2023-04-02 19:38:20 +0300

    PG-9 add seeds using Faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,52 +1,89 @@
-# require 'faker'
+class String
+    def red
+        "\e[31m#{self}\e[0m"
+    end
 
-# delete_data_and_puts_info = ->(model) do
-#     puts "Deleting #{model.name} data"
-#     model.delete_all
-# end
+    def green
+        "\e[32m#{self}\e[0m"
+    end
 
-# puts 'Deleting all data'
-# models = [User]
-# models.each { |model| delete_data_and_puts_info.call(model) }
+    def yellow
+        "\e[33m#{self}\e[0m"
+    end
 
+    def blue
+        "\e[34m#{self}\e[0m"
+    end
+end
+start_time = Time.now
 
-user = User.create(username: 'member1', email: 'member1@pagink.com')
-shelve = Shelve.create(title: 'Read books', author: user)
-author = Author.create(name: "Макс Кідрук")
-publisher = Publisher.create(name: 'Видавництво Бородатий Тамарин')
-book = Book.create(title: 'Колонія', pages_count: 904, isbn: '123456789', description: "Класна книжка", publisher: publisher)
-author.books << book
-shelve.books << book
-gink = Gink.create(title: "My first gink", text: 'Can you imagine this???', author: user)
-review = Review.create(text: 'My favourite book', author: user, book: book)
+delete_data_and_puts_info = ->(model) do
+    puts "Deleting #{model.name} data".yellow
+    model.delete_all
+end
 
-gink_comment = Comment.create(author: user, text: 'nice gink')
-review_comment = Comment.create(author: user, text: 'nice review')
-comment_comment = Comment.create(author: user, text: 'nice comment')
+puts 'Deleting all data'.yellow
+models = [User, Author, Gink, Review, Book, Shelve, Publisher, Comment, ImageLink, Like, Message, Notification, Raiting]
+models.each { |model| delete_data_and_puts_info.call(model) }
 
-gink.comments << gink_comment
-review.comments << review_comment
-review_comment.comments << comment_comment
+5.times do
+    user = User.create(Faker::Internet.user)
+    puts "User #{user.username.to_s.blue} created".red
 
-review_like = Like.create(user: user)
-comment_like = Like.create(user: user)
-gink_like = Like.create(user: user)
+    image_link = ImageLink.create(url: Faker::Internet.url)
+    user.image_link = image_link
+    puts 'Images links created'.red
 
-review.likes << review_like
-review_comment.likes << comment_like
-gink.likes << gink_like
+    author = Author.create(name: Faker::Book.author)
+    puts "Author #{author.name.to_s.blue} created".red
 
-sender = User.create(username: 'member2', email: "member2@pagink.com")
+    publisher = Publisher.create(name: Faker::Book.publisher)
+    puts "Publisher #{publisher.name.to_s.blue} created".red
+end
 
-message = Message.create(author: sender, recipient: user, text: 'Hello, nice to meet u')
-raiting = Raiting.create(author: user, book: book, value: 5)
-raiting = Raiting.create(author: sender, book: book, value: 2)
+15.times do
+    book = Book.create(title: Faker::Book.title, pages_count: Faker::Number.within(range: 100..3000), isbn: Faker::Number.number(digits: 10).to_s, description: Faker::Lorem.paragraph(sentence_count: 5), publisher: Publisher.all.sample)
+    puts "Book #{book.title.to_s.blue} created".red
 
-notification = Notification.create(title: 'You have new notification', payload: "1 new notification", user: user)
-book_image_link = ImageLink.create(url: 'link for book')
-user_image_link = ImageLink.create(url: 'link for user')
+    image_link = ImageLink.create(url: Faker::Internet.url)
+    book.image_link = image_link
+    puts 'Images links created'.red
 
-book.image_link = book_image_link
-user.image_link = user_image_link
+    shelve = Shelve.create(title: Faker::Quote.singular_siegler, author: User.all.sample)
+    shelve.books << Book.all.sample
+    puts "Shelve #{shelve.title.to_s.blue} created".red
+    
+    Author.all.sample.books << Book.all.sample
 
-puts 'Data created'
+    gink = Gink.create(title: Faker::Lorem.paragraph(sentence_count: 1), text: Faker::Lorem.paragraph(sentence_count: 4), author: User.all.sample)    
+    puts "Gink #{gink.title.to_s.blue} created".red
+
+    review = Review.create(text: Faker::Lorem.paragraphs(number: 2), author: User.all.sample, book: Book.all.sample)
+    puts "Review #{review.text.to_s.blue} created".red
+
+end
+
+30.times do
+    comment = Comment.create(author: User.all.sample, text: Faker::Lorem.paragraph(sentence_count: 3))
+    [Gink, Review, Comment].sample.all.sample.comments << comment
+    puts "Comment with id #{comment.id.to_s.blue} created".red
+
+    like = Like.create(user: User.all.sample)
+    [Gink, Review, Comment].sample.all.sample.likes << like
+    puts "Like with id #{like.id.to_s.blue} created".red
+end
+
+30.times do
+    sender = User.all.sample
+    message = Message.create(author: sender, recipient: (User.all - [sender]).sample, text: Faker::Lorem.paragraph(sentence_count: 1))
+    puts "Message with id #{message.id.to_s.blue} created".red
+
+    raiting = Raiting.create(author: User.all.sample, book: Book.all.sample, value: Faker::Number.within(range: 1..5))
+
+    puts "Raiting with #{raiting.id.to_s.blue} created".red
+
+    notification = Notification.create(title: Faker::Lorem.paragraph(sentence_count: 1), payload: Faker::Lorem.paragraph(sentence_count: 1), user: User.all.sample)
+    puts "Notification #{notification.title.to_s.blue} created".red
+end
+
+puts "Done in #{Time.now - start_time} seconds".green
