b189c3ae96b4c07151282d45fa3f3fd3221e975c
graveetone <zastavskijs@gmail.com>
2023-04-05 19:56:22 +0300

    PG-6 fix: add image_link to author and commentable as backref for comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
-    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-    has_many :likes, as: :likeable
-    has_many :comments, as: :commentable
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :likes, as: :likeable
+  has_many :comments, as: :commentable
+  belongs_to :commentable, polymorphic: true
 end
