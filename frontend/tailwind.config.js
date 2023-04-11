d4df2a612f138f8944759fae508df7b8e020b80e
graveetone <zastavskijs@gmail.com>
2023-04-11 23:56:58 +0300

    PG-16 responsive menu component with boilderplated react router

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ['public/*.html', './src/**/*.js', './src/**/*.jsx', './src/*.jsx'],
   theme: {
+    screens: {
+      'sm': '300px',
+      'md': '600px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
     extend: {},
   },
   plugins: [],
