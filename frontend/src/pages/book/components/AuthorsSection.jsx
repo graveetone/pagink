c359992c92f9da8ff2a156fc3d2d10d8c716bbd1
graveetone <zastavskijs@gmail.com>
2023-04-15 22:30:13 +0300

    PG-3 add book page improvements and fixes

--- frontend/src/pages/book/components/AuthorsSection.jsx
+++ frontend/src/pages/book/components/AuthorsSection.jsx
@@ -4,9 +4,9 @@ import AuthorCard from './AuthorCard'
 function AuthorsSection({ authors }) {
   return (
     <>
-      <div className='flex flex-col justify-center items-around b xs:w-3/4 sm:w-full md:w-1/2 mt-3'>
-        <p className='text-center text-gray-600 text-4xl'>Authors:</p>
-        <div className='flex flex-col items-center justify-center p-3 w-full'>
+      <div className='flex flex-col justify-center items-around xs:w-3/4 sm:w-full md:w-1/2'>
+        <p className='text-center text-gray-600 text-4xl my-4 p-1'>Authors</p>
+        <div className='flex flex-col items-center justify-center w-full'>
           {authors.map(author => {
             return (
               <AuthorCard key={`author-${author.id}`}
--- frontend/src/pages/book/components/BookInfo.jsx
+++ frontend/src/pages/book/components/BookInfo.jsx
@@ -12,43 +12,45 @@ function BookInfo({ data }) {
 
   return (
     <>
-      <div className='flex flex-col items-center justify-center h-full xs:w-full sm:w-full md:w-1/3'>
-
-        <table cellPadding={10} className='width-auto'>
-          <tr>
-            <td>
-              <span className='text-gray-600 text-xl'>ISBN: </span>
-            </td>
-            <td>
-              <span className='text-xl'>{data.isbn}</span>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <span className='text-gray-600 text-xl'>Pages: </span>
-            </td>
-            <td>
-              <span className='text-xl'>{data.pages_count}</span>
-            </td>
-          </tr>
-          <tr>
-            <td colSpan={2}>
-              <div className='flex justify-center items-center flex-col'>
-                <div className='flex justify-center items-center'>{filled_stars}{unfilled_stars}</div>
-                <div className='text-gray-600 text-lg'>based on <span className='font-black text-xl'>{data.raitings_count}</span> raitings</div>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td colSpan={2}>
-              <div className="flex justify-center items-center text-center">
-                <div className='text-gray-600 text-lg'>
-                  Users shelved this book <span className='font-black text-xl text-black'>{data.shelves_count}</span> times
+      <div className='flex flex-col items-center justify-center h-full xs:w-full sm:w-full md:w-1/2 '>
+        <p className='text-center text-gray-600 text-4xl p-2 my-4'>About this book</p>
+        <div className='w-3/4 rounded-3xl bg-gray-300 border-2 border-gray-400'>
+          <table cellPadding={12} className='w-full'>
+            <tr>
+              <td>
+                <span className='text-gray-600 text-xl'>ISBN: </span>
+              </td>
+              <td>
+                <span className='text-xl'>{data.isbn}</span>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <span className='text-gray-600 text-xl'>Pages: </span>
+              </td>
+              <td>
+                <span className='text-xl'>{data.pages_count}</span>
+              </td>
+            </tr>
+            <tr>
+              <td colSpan={2}>
+                <div className='flex justify-center items-center flex-col'>
+                  <div className='flex justify-center items-center'>{filled_stars}{unfilled_stars}</div>
+                  <div className='text-gray-600 text-lg'>based on <span className='font-black text-xl'>{data.raitings_count}</span> raitings</div>
                 </div>
-              </div>
-            </td>
-          </tr>
-        </table>
+              </td>
+            </tr>
+            <tr>
+              <td colSpan={2}>
+                <div className="flex justify-center items-center text-center">
+                  <div className='text-gray-600 text-lg'>
+                    Users shelved this book <span className='font-black text-xl text-black'>{data.shelves_count}</span> times
+                  </div>
+                </div>
+              </td>
+            </tr>
+          </table>
+        </div>
       </div >
     </>
   )
