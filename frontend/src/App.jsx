b328bc3b681c6e2b23e820f15ef890b0fbab6ec0
graveetone <zastavskijs@gmail.com>
2023-04-22 16:22:05 +0300

    PG-32 move routes to separate file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,46 +1,7 @@
-import {
-  Routes,
-  Route
-} from 'react-router-dom';
-
 import Menu from './components/menu/Menu'
 import LoginMenu from './components/menu/LoginMenu'
-import Header from './components/Header/Header';
-import { setPageTitle } from './helpers';
-import { BookPage, AuthorPage, ShelvePage, ShelvesPage, UserPage, LoginPage } from './pages'
 
-function Home() {
-  setPageTitle('Home')
-  return <Header />
-}
-
-function MainPage() {
-  setPageTitle('Main')
-  return <Header subheading={"Your bookshelf, your thoughts, your community"} />
-}
-
-function SignUpPage() {
-  setPageTitle('Sign Up')
-  return <Header subheading={"We're glad you're joining PagInk"} />
-}
-
-function Explore() {
-  setPageTitle('Explore')
-  return <Header heading='Explore' subheading={"Find new books"} />
-
-}
-function Settings() {
-  setPageTitle('Settings')
-  return (
-    <>
-      <Header heading='Settings' subheading={"Setup PagInk just for you"} />
-      <a href="/logout">Logout</a>
-    </>
-  )
-}
-function Logout() {
-  return <h1 className='text-6xl text-center'>Logout</h1>;
-}
+import { LoggedOutRoutes, LoggedInRoutes } from './routes';
 
 
 function App() {
@@ -48,33 +9,11 @@ function App() {
   return (
     <>
       <div className="flex flex-col items-center h-screen overflow-y-scroll pb-24">
-        {user ? (
-          <>
-              <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route path="/shelves" element={<ShelvesPage />} />
-                <Route path="/explore" element={<Explore />} />
-                <Route path="/settings" element={<Settings />} />
-                <Route path="/logout" element={<Logout />} />
-                <Route path="/authors" element={<AuthorPage />} />
-                <Route path="/shelve" element={<ShelvePage />} />
-                <Route path="/book" element={<BookPage />} />
-                <Route path="/user" element={<UserPage />} />
-              </Routes>
-          </>
-        ) : (
-          <>
-              <Routes>
-                <Route exact path="/" element={<MainPage />} />
-                <Route exact path="/login" element={<LoginPage />} />
-                <Route path="/sign_up" element={<SignUpPage />} />
-                <Route path="*" element={<LoginPage />} />
-              </Routes>
-          </>
-        )}
+        { user ? <LoggedInRoutes /> : <LoggedOutRoutes /> }
       </div >
+
       <div className='flex w-full justify-center'>
-        {user ? <Menu /> : <LoginMenu />}
+        { user ? <Menu /> : <LoginMenu /> }
       </div>
     </>
   );
