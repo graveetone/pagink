7aff5ea3b26c093af31792b579e09c41f6625c5f
graveetone <zastavskijs@gmail.com>
2023-04-22 16:47:45 +0300

    PG-34 ShelvesPage changes

--- frontend/src/pages/shelves_page/components/ShelveCard.jsx
+++ frontend/src/pages/shelves_page/components/ShelveCard.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 function ShelveCard({ shelve }) {
     return (
         <>
-            <NavLink to='/shelve' className='flex flex-col xs:w-full md:w-1/4 m-5'>
+            <NavLink to='/shelve' className='flex flex-col xs:w-full md:w-1/4 gap-1'>
                 <div className='flex justify-around items-end h-1/4 w-full'>
                     <div className='w-1/3 object-cover'>
                         <img src={shelve.books[0].image_url} alt={shelve.books[0].title} className="rounded-tl-md rounded-bl-md rounded-br-lg w-full md:w-full h-auto object-fill" />
@@ -16,9 +16,10 @@ function ShelveCard({ shelve }) {
                         <img src={shelve.books[2].image_url} alt={shelve.books[2].title} className="rounded-tl-md rounded-bl-md rounded-br-lg w-full md:w-full h-auto object-fill" />
                     </div>
                 </div>
-
-                <div className='bg-black w-full h-4 shadow-xl rounded-lg shadow-black my-1' />
-                <p className='text-center text-xl'>{`${shelve.title} (${shelve.books.length})`}</p>
+                <div className='flex flex-col gap-3'>
+                    <div className='bg-black w-full h-3 shadow-lg rounded-lg shadow-black' />
+                    <p className='text-center text-xl'>{`${shelve.title} (${shelve.books.length})`}</p>
+                </div>
             </NavLink>
         </>
     );
