eed2ab042f056c23b9c3fbf47cbd418bd421f8b1
graveetone <zastavskijs@gmail.com>
2023-04-23 00:38:41 +0300

    PG-41 hide menu on click outside/mouse leave

--- frontend/src/components/menu/Menu.jsx
+++ frontend/src/components/menu/Menu.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import MenuItem from '../menu_item/MenuItem'
 import icons from '../../components/icons'
+import { useState, useEffect, useRef } from 'react';
 
 function Menu() {
+    const [isMenuVisible, setIsMenuVisible] = useState(false)
+    const divRef = useRef()
+
+    useEffect(() => {
+        function hideMenu(event) {
+            if (isMenuVisible && divRef.current && !divRef.current.contains(event.target)) {
+                setIsMenuVisible(false)
+            }
+        }
+
+        document.addEventListener('mousedown', hideMenu);
+    }, [isMenuVisible])
+
     const captions = {
         uk: {
             home: "Головна",
@@ -22,14 +36,28 @@ function Menu() {
 
     const locale = 'en'
     return (
-        <div className='flex xs:w-full md:w-3/4 lg:w-3/4 h-1/7 bottom-0 justify-evenly fixed sm:gap-2'>
-            <MenuItem key='1' icon={icons.home} caption={captions[locale].home} url={'/'}></MenuItem>
-            <MenuItem key='2' icon={icons.shelves} caption={captions[locale].shelves} url={'/shelves'}></MenuItem>
-            <MenuItem key='5' icon={icons.user} caption={captions[locale].profile} url={'/user'}></MenuItem>
-            <MenuItem key='3' icon={icons.explore} caption={captions[locale].explore} url={'/explore'}></MenuItem>
-            <MenuItem key='4' icon={icons.settings} caption={captions[locale].settings} url={'/settings'}></MenuItem>
-        </div>
+        <>
+            {
+                isMenuVisible ? (
 
+                    <div ref={divRef} className={'flex xs:w-full md:w-3/4 lg:w-3/4 h-1/7 bottom-0 justify-evenly fixed sm:gap-2'}
+                        onMouseLeave={() => setIsMenuVisible(false)}>
+                        <MenuItem key='1' icon={icons.home} caption={captions[locale].home} url={'/'}></MenuItem>
+                        <MenuItem key='2' icon={icons.shelves} caption={captions[locale].shelves} url={'/shelves'}></MenuItem>
+                        <MenuItem key='5' icon={icons.user} caption={captions[locale].profile} url={'/user'}></MenuItem>
+                        <MenuItem key='3' icon={icons.explore} caption={captions[locale].explore} url={'/explore'}></MenuItem>
+                        <MenuItem key='4' icon={icons.settings} caption={captions[locale].settings} url={'/settings'}></MenuItem>
+                    </div>
+                ) : (
+                    <div className={'flex xs:w-1/2 sm:w-1/3 md:w-1/5 lg:w-3/4 bottom-0 fixed'} onMouseEnter={() => setIsMenuVisible(true)} >
+                        <div className='flex justify-center items-center rounded-t-2xl bg-black xs:px-12 md:px-24 py-2 flex-grow'>
+                            <div className='rounded-full bg-ruby-red py-1 flex-grow'>
+                            </div>
+                        </div>
+                    </div >
+                )
+            }
+        </>
     )
 }
 
