fc80533c9b84994b794d3fbc9be9544f850e5e0b
graveetone <zastavskijs@gmail.com>
2023-04-26 23:56:34 +0300

    PG-47 fix: popup display above the main content

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -4,7 +4,7 @@ import Title from './components/Title'
 
 function Header({ heading, subheading }) {
     return (
-        <div className='flex flex-col justify-start items-center gap-5 w-full'>
+        <div className='flex -z-10 flex-col justify-start items-center gap-5 w-full'>
             <div className='flex md:flex-row flex-col justify-center items-center w-full gap-5'>
                 {heading ? (
                     <Title text={heading} />
