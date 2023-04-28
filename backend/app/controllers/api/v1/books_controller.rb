7a744862e2b3e799224a77b0ce4e0b6b3053ace7
graveetone <zastavskijs@gmail.com>
2023-04-28 18:48:54 +0300

    PG-13 add create book by google books volume id

--- backend/app/controllers/api/v1/books_controller.rb
+++ backend/app/controllers/api/v1/books_controller.rb
@@ -18,9 +18,45 @@ class Api::V1::BooksController < ApplicationController
   end
 
   def create
+    response = case book_params[:source][:type]
+    when 'GB_API'
+      respond_with_gb_api
+    when 'MANUAL'
+      render json: 'To be done'
+    else
+      render json: PaginkErrorBuilder.new(:wrong_book_source_type).render
+    end
   end
 
   def search
-    # GoogleAPIService.search
+    if search_params[:global]
+      books = GoogleBooksSearchService.new(
+        query: search_params[:value],
+        page: search_params[:page] || 1
+      ).search
+
+      render json: books
+    else
+      books = Book.search(search_params[:value])
+      render json: books, each_serializer: SearchBookSerializer
+    end
+  end
+
+  private
+
+  def respond_with_gb_api
+    response = GoogleBooksBookCreatorService.new(
+      book_params[:source][:id]
+    ).get_book
+
+    render json: response
+  end
+
+  def book_params
+    params.require(:book).permit(source: [:type, :id])
+  end
+
+  def search_params
+    params.require(:query).permit(:value, :global, :page)
   end
 end
