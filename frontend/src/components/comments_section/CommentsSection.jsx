c911d2a4a5a757a76ffd5d901d44b2f27029dc83
graveetone <zastavskijs@gmail.com>
2023-04-30 22:03:20 +0300

    PG-15 layout fixes

--- frontend/src/components/comments_section/CommentsSection.jsx
+++ frontend/src/components/comments_section/CommentsSection.jsx
@@ -1,4 +1,5 @@
 import React, { useRef } from 'react'
+
 import CommentCard from './CommentCard'
 
 function CommentsSection({ comments }) {
@@ -9,7 +10,7 @@ function CommentsSection({ comments }) {
     }
 
     return (
-        <div className='flex flex-col justify-center items-center gap-12 m-6'>
+        <div className='flex flex-col justify-center items-center gap-12 w-5/6 md:w-1/2'>
             {comments.map(comment => {
                 const parent = findCommentById(comment.parentId);
                 return <CommentCard key={comment.id}  comment={comment} parent={parent} commentsRef={commentsRef} />
