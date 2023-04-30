8d512260fb946fd2b91d965ff4cbe4625f50ea2a
graveetone <zastavskijs@gmail.com>
2023-04-30 22:02:47 +0300

    PG-15 ginks/reviews for UserPage

--- frontend/src/pages/user_page/data.js
+++ frontend/src/pages/user_page/data.js
@@ -1,11 +1,71 @@
-export const data = {
-    user: {
+export const data = [
+    {
         id: 1,
-        username: "graveetone",
-        email: "parker_reinger@sporer.com",
-        last_seen: null,
-        status: "ACTIVE",
-        image_url: "https://lh3.googleusercontent.com/a/AGNmyxbEQfvSPsOPbiim51eu0juOcPUg0bCrHbLkjWE8=s288",
-        created_at: "2023-04-07T19:48:09.367Z"
+        type: 'gink',
+        author: {
+            username: 'graveetone',
+            image_url: "https://drive.google.com/u/0/uc?id=1D13oE__ZmhfieSE6SkRInSDJw831y6sz&export=download",
+        },
+        likedByCurrentUser: true,
+        title: "Новий ДЖИНК",
+        text: `Люблю читати цікаві книги`,
+        created_at: '15 minutes ago',
+        likesCount: 123,
+        commentsCount: 9
+    },
+    {
+        id: 2,
+        type: 'review',
+        author: {
+            username: 'graveetone',
+            image_url: "https://drive.google.com/u/0/uc?id=1D13oE__ZmhfieSE6SkRInSDJw831y6sz&export=download",
+        },
+        likedByCurrentUser: true,
+        text: `Ви уже читали цей шедевр?`,
+        book: {
+            id: 1,
+            title: "Згодом",
+            image_url: "https://bookclub.ua/images/db/goods/57769_113287.jpg",
+            authors: [
+                {
+                    id: 2,
+                    name: "Стівен Кінг",
+                },
+                {
+                    id: 3,
+                    name: "Stephen King",
+                }
+            ]
+        },
+        created_at: '7 minutes ago',
+        likesCount: 60,
+        commentsCount: 0
+    },
+    {
+        id: 3,
+        type: 'gink',
+        author: {
+            username: 'graveetone',
+            image_url: "https://drive.google.com/u/0/uc?id=1D13oE__ZmhfieSE6SkRInSDJw831y6sz&export=download",
+        },
+        likedByCurrentUser: true,
+        title: "Кайдашева сім'я",
+        text: 'Я написав щось класне!',
+        created_at: '145 years ago',
+        likesCount: 12,
+        commentsCount: 1
+    },
+    {
+        id: 4,
+        type: 'gink',
+        author: {
+            username: 'graveetone',
+            image_url: "https://drive.google.com/u/0/uc?id=1D13oE__ZmhfieSE6SkRInSDJw831y6sz&export=download",
+        },
+        likedByCurrentUser: true,
+        text: 'Зайшов в книгарню, вийшов з книгарні - купив книжки!',
+        created_at: '145 years ago',
+        likesCount: 12,
+        commentsCount: 4
     }
-}
+]
