6492e1154779a79410a8b1fe5335db3d20bd53ff
graveetone <zastavskijs@gmail.com>
2023-05-06 19:15:38 +0300

    PG-52 image fixes

--- frontend/src/components/BookImage.jsx
+++ frontend/src/components/BookImage.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 
 function BookImage({ src, alt, width, height }) {
     return (
-        <div className={`flex items-center justify-center m-3 ${width} ${height}`}>
-            <img src={src} alt={alt} className={'rounded-3xl w-full h-full object-cover object-center'} />
+        <div className={`flex items-center justify-center w-full h-full`}>
+            <img src={src} alt={alt} className={`rounded-3xl ${width} ${height} object-cover object-center`} />
         </div>
-        
     )
 }
 
--- frontend/src/components/Image.jsx
+++ frontend/src/components/Image.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 
 function Image({ src, alt, width, height }) {
     return (
-        <div className={`flex items-center justify-center m-3 ${width} ${height}`}>
-            <img src={src} alt={alt} className={'rounded-full w-full h-full object-cover object-center'} />
+        <div className={`flex items-center justify-center w-full h-full`}>
+            <img src={src} alt={alt} className={`rounded-full ${width} ${height} object-cover object-center`} />
         </div>
-        
     )
 }
 
