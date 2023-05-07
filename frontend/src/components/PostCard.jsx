117968dfb6f472c854d1fba0b0e5aba7e1d2a1fd
graveetone <zastavskijs@gmail.com>
2023-05-07 04:27:01 +0300

    PG-61 review, gink and general post cards + icons

--- frontend/src/components/PostCard.jsx
+++ frontend/src/components/PostCard.jsx
@@ -1,59 +1,15 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react'
 
-import Image from './../components/Image'
-import BookImage from './../components/BookImage'
-import Button from './Button'
-import icons from './icons'
-import helpers from '../helpers'
+import ReviewCard from './ReviewCard'
+import GinkCard from './GinkCard'
 
 function PostCard({ post }) {
-  const [liked, setLiked] = useState(post.likedByCurrentUser)
-  const linkToPost = (post.type === 'gink' ? `/gink/${post.id}` : `/review/${post.id}`)
-  return (
-    <>
-      <Link to={linkToPost} className='shadow-2xl p-1 shadow-black flex w-full justify-center items-center border-black text-center rounded-xl'>
-        <div className='flex flex-col w-full items-center'>
-          <div className='flex w-full justify-start'>
-            <div>
-              <div className='flex flex-col p-2 font-balsamiq'>
-                <p className='text-xl'>{post.author.username}</p>
-                <Link to={`user/${post.author.id}`} className='flex'>
-                  <Image src={post.author.image_url} alt={post.author.username} width={'w-24'} height={'h-24'} />
-                </Link>
-                <p className='text-sm'>{post.timestamp}</p>
-              </div>
+  const cardByPostType = {
+    gink: <GinkCard gink={post} />,
+    review: <ReviewCard review={post} />
+  }
 
-              {post.book &&
-                <div className='flex justify-center border-t-2 border-black items-center flex-col font-balsamiq'>
-                  <Link to={`/book/${post.book.id}`} className='flex'>
-                    <BookImage src={post.book.image_url} alt={post.book.title} width={'w-24'} height={'h-36'} />
-                  </Link>
-                  <p className='text-xl'>{post.book.title}</p>
-                </div>
-              }
-            </div>
-            <div className='flex flex-col items-center justify-between w-full gap-3 font-balsamiq p-2 border-l-2 border-black'>
-              <div className='flex flex-col justify-center items-center w-full'>
-                {post.title && <p>{post.title}</p>}
-                <div className='flex text-xl p-3 justify-center w-full overflow-hidden items-center text-center font-sans'>
-                  {helpers.getTextPreview(post.text)}
-                </div>
-              </div>
-              <div className='flex self-end justify-end items-end gap-6'>
-                <div className='flex justify-center items-center'>
-                  <Button icon={<>{post.likes_count}{liked ? icons.liked : icons.unliked}</>} onClick={() => { setLiked(!liked) }} />
-                </div>
-                <div className='flex justify-center items-center'>
-                  <Button icon={<>{post.comments_count} {icons.comments}</>} onClick={() => { }} />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </Link>
-    </>
-  )
+  return cardByPostType[post.type]
 }
 
 export default PostCard;
