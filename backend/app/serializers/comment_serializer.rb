dd72cf8cbd5a0dfd99684c3148608a3f1f74679d
graveetone <zastavskijs@gmail.com>
2023-05-07 04:24:28 +0300

    PG-61 comment origin

--- backend/app/serializers/comment_serializer.rb
+++ backend/app/serializers/comment_serializer.rb
@@ -1,5 +1,5 @@
 class CommentSerializer < BaseSerializer
-  attributes %i[id author likedBy text timestamp]
+  attributes %i[id author likedBy text timestamp origin]
 
   def likes_count
     object.likes.count
@@ -15,5 +15,17 @@ class CommentSerializer < BaseSerializer
 
   def likedBy
     object.likes.pluck(:user_id)
+  end
+
+  def origin
+    origin_comment = object.origin
+
+    return unless origin_comment
+
+    {
+      id: origin_comment.id,
+      text: origin_comment.text,
+      authorUsername: origin_comment.author.username
+    }
   end
 end
