f93ea3d55c4fc3103c91f2265d26491c874dff6b
graveetone <zastavskijs@gmail.com>
2023-05-11 12:55:28 +0300

    PG-70 perform current_user request only if token is present

--- frontend/src/api/paginkQuery.js
+++ frontend/src/api/paginkQuery.js
@@ -3,12 +3,12 @@ import axios from "axios";
 import helpers from "../helpers";
 
 
-function usePaginkQuery(key, url, options) {
+function usePaginkQuery(key, url, options, queryOptions) {
   const token = helpers.getTokenFromSession()
   return useQuery(key, async () => {
     const response = await axios.get(url, { ...options, headers: { Authorization: token } });
     return response.data;
-  });
+  }, queryOptions);
 }
 
 export default usePaginkQuery;
